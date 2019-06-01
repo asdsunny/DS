11fde73404c39c41b3cdcd62a8a94b7889fdcd5b
asdsunny <395359511@qq.com>
2019-06-01 18:04:39 +0800

    LinkedList

---
+++ src/MyLinkList.java
@@ -0,0 +1,22 @@
+
+public class MyLinkList <E>{
+	
+	private class Node{
+		E e;
+		Node next;
+		
+		public Node() {
+			
+		}
+	}
+	public int size;
+	public Node head;
+	
+	public int getSize() {
+		return size;
+	}
+	public boolean isEmpty() {
+		return size==0;
+	}
+
+}
